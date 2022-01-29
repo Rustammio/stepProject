60c830ff413ff958a71ea14a8d05ad805bf5fa1f
MariiaSl <mvs030915@gmail.com>
2022-01-29 23:34:06 +0200

    burger button changed, js added

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
-// const btnMenu = document.querySelector(".nav__burger-menu");
-// const menu = document.querySelector(".nav__content-wrapper");
+const btnMenu = document.querySelector(".nav__burger-menu");
+const btnLines = document.querySelector(".nav__burger-lines");
+const menu = document.querySelector(".nav__content-wrapper");
 
-// btnMenu.addEventListener("click", () => {
-//   btnMenu.classList.toggle("nav__burger-menu--open");
-//   menu.classList.toggle("nav__content-wrapper--open");
-// });
+btnMenu.addEventListener("click", () => {
+  btnLines.classList.toggle("nav__burger-lines--active");
+  menu.classList.toggle("nav__content-wrapper--open");
+});
 
-console.log("Hi");
+document.querySelector(".modal-container").addEventListener("click", () => {
+  btnLines.classList.remove("nav__burger-lines--active");
+  menu.classList.remove("nav__content-wrapper--open");
+});
